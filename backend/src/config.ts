7cafc76cdb5ff783e51458a205a35473fc72c0dd
afrit-bagani <o6228466@gmail.com>
2025-08-05 09:31:15 +0530

    Add frontend-url from .env

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,2 +1,4 @@
+export const FRONTEND_URL = process.env.FRONTEND_URL;
+
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET!;
 export const ACCESS_TOKEN_EXPIRY = parseInt(process.env.ACCESS_TOKEN_EXPIRY!);
