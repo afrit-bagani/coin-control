f5ee286c03fbcfbe32982cd41259b46a2242692b
afrit-bagani <o6228466@gmail.com>
2025-08-07 10:00:00 +0530

    remove dotenv config() fn, env will be load from start off the project, package.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,7 @@
-import { config } from "dotenv";
-import { resolve } from "path";
 import { PrismaClient } from "@prisma/client";
 
 // Local import
 import { app } from "./server";
-
-const envFile =
-  process.env.NODE_ENV === "production"
-    ? ".env.production"
-    : ".env.development";
-
-config({ path: resolve(process.cwd(), envFile) });
 
 export const prisma = new PrismaClient();
 
@@ -19,13 +10,13 @@ const PORT = process.env.PORT || 3000;
 (async function main() {
   try {
     await prisma.$connect();
-    console.log("🐘 Connected to postgreSQL...");
+    console.log("📂  Connected to Database...");
     app.listen(PORT, () => {
       console.log(
-        `🔆 Server running in ${process.env.NODE_ENV} on port ${PORT}`
+        `🌐  Server running in ${process.env.NODE_ENV} on port: ${PORT}`
       );
     });
   } catch (error) {
-    console.error(`⚠️ Error while starting server: \n`, error);
+    console.error(`❌❌❌❌  Error while starting server: \n`, error);
   }
 })();
