38b7c7e5b4330b1c4a75de714f8c6d3047dd67be
afrit-bagani <o6228466@gmail.com>
2025-08-07 10:00:00 +0530

    add REST method

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,13 @@ import { authenticate } from "./middleware/auth.middleware";
 
 export const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 
 app.use("/auth", authRouter);
